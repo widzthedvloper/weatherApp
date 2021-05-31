794d66c7b29ab7210930420c30236026d86cdc51
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-05-31 17:44:54 -0400

    Create apiInfo function to retrieve data from api

---
+++ src/data.js
@@ -0,0 +1,17 @@
+const apiInfo = (location) => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c7911001fbb406aa98e26c70ab98ec78`, { mode: 'cors' })
+    .then((api) => (api.json()))
+    .then((api) => {
+      const temperature = Math.round((api.main.temp) - 273);
+      const city = api.name;
+      const { humidity } = api.main;
+      const minTemp = Math.round((api.main.temp_min) - 273);
+      const maxTemp = Math.round((api.main.temp_max) - 273);
+      const { description } = api.weather[0];
+      // eslint-disable-next-line no-console
+      console.log(temperature, city, humidity, minTemp, maxTemp, description);
+    })
+    .catch((error) => error);
+};
+
+export default apiInfo;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+import apiInfo from './data';
+
+apiInfo('miami');
