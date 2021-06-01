a5bd8e00fa1f181bea174359251a748d527691d6
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-06-01 13:18:39 -0400

    Create render info function

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,4 +1,21 @@
 const renderInfo = (temperature, city, humidity, minTemp, maxTemp, description) => {
   const temp = document.getElementById('main-temp');
   temp.innerHTML = temperature;
+
+  const theCity = document.getElementById('city');
+  theCity.innerHTML = city;
+
+  const theHumidity = document.getElementById('humidity');
+  theHumidity.innerHTML = humidity;
+
+  const theMinTemp = document.getElementById('temp_min');
+  theMinTemp.innerHTML = minTemp;
+
+  const theMaxTemp = document.getElementById('temp_max');
+  theMaxTemp.innerHTML = maxTemp;
+
+  const theDescription = document.getElementById('weather-description');
+  theDescription.innerHTML = description;
 };
+
+export default renderInfo;
